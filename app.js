6e9c3149c61718f30cbccffa41ca812c1677fedb
Musab74 <rmcbaxt@gmail.com>
2024-11-09 02:17:44 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+let tasks = []; // Declare tasks first
+
+// Load tasks from localStorage when the document is ready
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (storedTasks) {
+        storedTasks.forEach((task) => tasks.push(task));
+        updateTasksList();
+        updateStats();
+    }
+});
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const addTask = () => {
+    const taskInput = document.getElementById("task");
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text: text, completed: false });
+        taskInput.value = "";
+        updateTasksList();
+        updateStats();
+        saveTasks();
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const task = document.getElementById('task');
+    task.value = tasks[index].text;
+
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = totalTasks > 0 ? (completeTasks / totalTasks) * 100 : 0;
+
+    const progressbar = document.getElementById("progress");
+    progressbar.style.width = `${progress}%`;
+    document.getElementById("numbers").innerText = `${completeTasks}/${totalTasks}`;
+
+    if (tasks.length > 0 && completeTasks === totalTasks) {
+        blaskConfetti();
+    }
+};
+
+const updateTasksList = () => {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = ""; // Clear the task list
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+            <div class="taskItem">
+                <div class="task ${task.completed ? 'completed' : ''}">
+                    <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+                    <p>${task.text}</p>
+                </div>
+                <div class="icons">
+                    <img src="./img/edit2.png" onClick="editTask(${index})"/>
+                    <img src="./img/delete1.png" onClick="deleteTask(${index})"/>
+                </div>
+            </div>
+        `;
+        listItem.querySelector(".checkbox").addEventListener("change", () => toggleTaskComplete(index));
+        taskList.append(listItem);
+    });
+};
+
+document.getElementById("submit").addEventListener("click", function(e) {
+    e.preventDefault();
+    addTask();
+});
+
+const blaskConfetti = () => {
+    const defaults = {
+        spread: 360,
+        ticks: 100,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["heart"],
+        colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+    };
+
+    confetti({
+        ...defaults,
+        particleCount: 50,
+        scalar: 2,
+    });
+
+    confetti({
+        ...defaults,
+        particleCount: 25,
+        scalar: 3,
+    });
+
+    confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 4,
+    });
+};
